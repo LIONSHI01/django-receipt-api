d78b827dcaf1e7ea38c75f7d0df7083511712471
lionc <shizijian2010@yahoo.com.hk>
2023-12-24 10:02:45 +0800

    update files

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -62,7 +62,8 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload['email'])
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']).exists()
         self.assertFalse(user_exists)
 
     def test_create_token_for_user(self):
@@ -127,7 +128,7 @@ class PrivateUserApiTests(TestCase):
         self.user = create_user(
             name='Test Name',
             email='test@example.com',
-            password='goodpass',
+            password='testpass123',
         )
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
@@ -149,10 +150,7 @@ class PrivateUserApiTests(TestCase):
 
     def test_update_user_profile(self):
         """Test updating the user profile for the authenticated user."""
-        payload = {
-            'name': 'updated name',
-            'password': 'badpass',
-        }
+        payload = {'name': 'Updated name', 'password': 'newpassword123'}
         res = self.client.patch(ME_URL, payload)
         # By default, db not refresh automatically
         self.user.refresh_from_db()
