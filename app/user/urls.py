60489d26c519470972d697454fb2eae33e5a70d0
lionc <shizijian2010@yahoo.com.hk>
2023-12-24 09:48:29 +0800

    add User Manage Views

--- app/user/urls.py
+++ app/user/urls.py
@@ -9,5 +9,7 @@ from user import views
 # NOTE: define app_name and url so in tests file : CREATE_USER_URL = reverse('user:create') could get the same url
 app_name = 'user'
 urlpatterns = [
-    path('create/', views.CreateUserView.as_view(), name='create')
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
 ]
